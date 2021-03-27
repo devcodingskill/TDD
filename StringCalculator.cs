4a19937c6911d3bda3ce705a624371f49a939e67
Tan <yutthana.tunti@gmail.com>
2021-03-27 21:52:48 +1000

    first commit

---
+++ StringCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDDTest
+{
+    internal class StringCalculator
+    {
+        public static int Add(string input)
+        {
+            if(input == null)
+            return 0;
+            List<int> nagetiveNumber = new List<int>();
+            if (input.Contains(","))
+            {
+                int result = 0;
+                var t = input.Split(',');
+                foreach (var tt in t)
+                {
+                    var temp =Int32.TryParse(tt, out Int32 a) ? a : 0;
+                    if (temp < 0)
+                        nagetiveNumber.Add(temp);
+                    else
+                        result += temp;
+                }
+                if (nagetiveNumber.Count > 0)
+                {
+                    var exceptionMessage = $"Negative numbers not allowed: {nagetiveNumber}.";
+                    throw new ArgumentException(exceptionMessage);
+                }
+                else
+                    return result;
+            }
+            return Int32.TryParse(input, out Int32 j) ? j : 0;
+            
+        }
+    }
+}
---
+++ StringCalculatorKata.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TDDTest
+{
+    public class StringCalcutorKata
+    {
+
+        [Test]
+        public void Add_EmptyStringAsParam_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(string.Empty));
+        }
+        [Test]
+        public void Add_StringContainingSingleNumber_ReturnsTheNumberItself()
+        {
+
+            var input = "5";
+            var expectResult = 5;
+            Assert.AreEqual(expectResult, StringCalculator.Add(input));
+        }
+
+        [Test]
+        public void Add_TwoNumbersSeparatedByComma_ReturnsTheirSum()
+        {
+            var numbers = "7,8";
+            var expectedResult = 15;
+            Assert.AreEqual(expectedResult, StringCalculator.Add(numbers));
+        }
+        [Test]
+        public void Add_ThreeNumbersSeparatedByComma_ReturnsTheirSum()
+        {
+            var numbers = "1, 2, 3";
+            var expected = 6;
+            Assert.AreEqual(expected, StringCalculator.Add(numbers));
+        }
+
+        [TestCase("1,2,3,4", 10)]
+        [TestCase("8,7,20", 35)]
+        [TestCase("5,0,4,1001", 1010)]
+        [TestCase("5,0,4,1000", 1009)]
+        [TestCase("26,6,90", 122)]
+        public void Add_MoreThanThreeNumbersSeparatedByComma_ReturnsTheirSum(string input, int result)
+        {
+            Assert.AreEqual(result, StringCalculator.Add(input));
+        }
+
+        [TestCase("1,2,3,4,5,-5")]
+        [TestCase("-1,1,2,9")]
+        [TestCase("5,6,8,-5")]
+        public void Add_StringContainingNegativeNumbers_Throws(string numbers)
+        {
+            Assert.Throws<ArgumentException>(() => StringCalculator.Add(numbers));
+        }
+    }
+}
